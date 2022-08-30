c058928bb2e542e02858b09ce565c885a58cb6d5
Shashank-6storage <111342039+Shashank-6storage@users.noreply.github.com>
2022-08-30 12:08:22 +0530

    commented

--- lib/ratemanager-pipeline-stack.ts
+++ lib/ratemanager-pipeline-stack.ts
@@ -8,8 +8,9 @@ export class RateManagerPipeLineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, context: CDKContext, props?: cdk.StackProps) {
         super(scope, id, props);
         
+        console.log(`pipeline creation started`);
         const pipeline = new CodePipeline(this, 'pipeline', {
-            pipelineName: `ratemanager-${context.environment}-pipeline`,
+            pipelineName: `ratemanager-${context.environment}-pipeline-fortest`,
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.connection('Shashank-6storage/RateManager_CDK', 
                 'develop',
