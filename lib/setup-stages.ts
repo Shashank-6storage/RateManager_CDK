0cf92dba839f1cf8f87dbdd68547930896018901
Shashank-6storage <111342039+Shashank-6storage@users.noreply.github.com>
2022-09-01 10:27:43 +0530

    test commit

--- lib/setup-stages.ts
+++ lib/setup-stages.ts
@@ -7,7 +7,7 @@ export class RatemanagerPipeLineStages extends cdk.Stage{
     constructor(scope: Construct, stageName: string, context: CDKContext) {
         super(scope, stageName);
 
-    const RMlambdaStack = new RatemanagerLambdaStack(this, stageName, context);
+    const lambdaStack = new RatemanagerLambdaStack(this, stageName, context);
         
     }
 }
