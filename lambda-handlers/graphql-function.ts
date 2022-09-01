40fcf91be629b784447cd0b34ae35ab95f03ee66
Shashank-6storage <111342039+Shashank-6storage@users.noreply.github.com>
2022-09-01 18:56:21 +0530

    test

--- lambda-handlers/graphql-function.ts
+++ lambda-handlers/graphql-function.ts
@@ -1,22 +1,36 @@
-import { ApolloServer, gql } from 'apollo-server-lambda';
+// import { ApolloServer, gql } from 'apollo-server-lambda';
 
-const typeDefs = gql`
-  type Query {
-    user: User
-  }
+// const typeDefs = gql`
+//   type Query {
+//     user: User
+//   }
 
-  type User {
-    id: ID
-    name: String
-  }
-`;
+//   type User {
+//     id: ID
+//     name: String
+//   }
+// `;
 
-const resolvers = {
-  Query: {
-    user: () => ({ id: 123, name: 'John Doe' })
-  }
+// const resolvers = {
+//   Query: {
+//     user: () => ({ id: 123, name: 'John Doe' })
+//   }
+// };
+
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// exports.handler = server.createHandler();
+
+
+import { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event, context) => {
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      // web hook lambda handler code goes here
+      return resolve('This is a graphql Function');
+    } catch (error) {
+      reject();
+    }
+  });
 };
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-exports.handler = server.createHandler();
