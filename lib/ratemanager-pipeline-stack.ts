4edb1e1dba4923b0d0089bd9868ea311d5990a53
Shashank-6storage <111342039+Shashank-6storage@users.noreply.github.com>
2022-09-02 19:31:44 +0530

    test commit

--- lib/ratemanager-pipeline-stack.ts
+++ lib/ratemanager-pipeline-stack.ts
@@ -9,16 +9,18 @@ export class RateManagerPipeLineStack extends cdk.Stack {
         constructor(scope: any, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        const source = CodePipelineSource.connection('Shashank-6storage/RateManager_CDK', 
+        'develop',
+        {
+            connectionArn: 
+                "arn:aws:codestar-connections:ap-south-1:760389274302:connection/2c3f1bd5-3ff4-4546-a42d-4f85f0a408cd"
+        });
+
         //console.log(`env is : ${JSON.stringify(context)}`);
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: `ratemanager-cicd-pipeline`,
             synth: new ShellStep('Synth', {
-                input: CodePipelineSource.connection('Shashank-6storage/RateManager_CDK', 
-                'develop',
-                {
-                    connectionArn: 
-                        "arn:aws:codestar-connections:ap-south-1:760389274302:connection/2c3f1bd5-3ff4-4546-a42d-4f85f0a408cd"
-                }),
+                input: source,
                 commands: [
                     'npm ci',
                     'npm run build',
@@ -42,6 +44,7 @@ export class RateManagerPipeLineStack extends cdk.Stack {
         }));
 
         devstage.addPost(new ShellStep('validate', {
+            input: source,
             commands: ['npm test']
           }));
         devstage.addPost(new ManualApprovalStep(`Manual approval before test`));
