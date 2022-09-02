3253e30f93f8859403a496a91eee184460519355
Shashank-6storage <111342039+Shashank-6storage@users.noreply.github.com>
2022-09-02 20:00:24 +0530

    validate tests

--- lib/ratemanager-pipeline-stack.ts
+++ lib/ratemanager-pipeline-stack.ts
@@ -9,24 +9,24 @@ export class RateManagerPipeLineStack extends cdk.Stack {
         constructor(scope: any, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const source = CodePipelineSource.connection('Shashank-6storage/RateManager_CDK', 
-        'develop',
-        {
-            connectionArn: 
-                "arn:aws:codestar-connections:ap-south-1:760389274302:connection/2c3f1bd5-3ff4-4546-a42d-4f85f0a408cd"
+        const synthstep = new ShellStep('Synth', {
+            input: CodePipelineSource.connection('Shashank-6storage/RateManager_CDK', 
+            'develop',
+            {
+                connectionArn: 
+                    "arn:aws:codestar-connections:ap-south-1:760389274302:connection/2c3f1bd5-3ff4-4546-a42d-4f85f0a408cd"
+            }),
+            commands: [
+                'npm ci',
+                'npm run build',
+                'npx cdk synth'
+            ]
         });
 
         //console.log(`env is : ${JSON.stringify(context)}`);
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: `ratemanager-cicd-pipeline`,
-            synth: new ShellStep('Synth', {
-                input: source,
-                commands: [
-                    'npm ci',
-                    'npm run build',
-                    'npx cdk synth'
-                ]
-            })
+            synth: synthstep
         });
 
         const devcontext: CDKContext = {
@@ -43,9 +43,9 @@ export class RateManagerPipeLineStack extends cdk.Stack {
             }
         }));
 
-        devstage.addPost(new ShellStep('validate', {
-            input: source,
-            commands: ['npm test']
+        devstage.addPost(new ShellStep('validate tests', {
+            input: synthstep,
+            commands: ['node tests/webhook.tests.js']
           }));
         devstage.addPost(new ManualApprovalStep(`Manual approval before test`));
         
