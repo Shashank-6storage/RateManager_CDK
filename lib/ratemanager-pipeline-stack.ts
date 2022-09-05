d97d52e3423601892069f10ce0ff0eb2c6dbc5e0
Shashank-6storage <111342039+Shashank-6storage@users.noreply.github.com>
2022-09-05 15:53:01 +0530

    last test

--- lib/ratemanager-pipeline-stack.ts
+++ lib/ratemanager-pipeline-stack.ts
@@ -53,7 +53,7 @@ export class RateManagerPipeLineStack extends cdk.Stack {
         // });
         
         devstage.addPost(new ShellStep('validate tests', {
-            input: synthstep,
+            input: source,
             commands: ['npm t']
           }));
         devstage.addPost(new ManualApprovalStep(`Manual approval before test`));
